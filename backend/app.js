e0cf4087fadccd1df5da75d4aed868d0f5ef8389
Krishnam Rathi <krishnam.rathi@gmail.com>
2021-01-02 19:04:30 +0530

    Express server setup

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,35 @@
-var http = require('http');
 var mysql = require('mysql');
+var express = require("express");
+const dotenv = require('dotenv');
+dotenv.config();
 
-const port = 8000;
+const app = express();
+const port = process.env.PORT || 8080;
+const host = process.env.HOST || "localhost";
 
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "password",
-    insecureAuth : true
+    insecureAuth: true,
+    databse: "oldAge"
 });
+
 
 con.connect(function (err) {
     if (err) throw err;
     console.log("Database Connected!");
+    // con.query("CREATE DATABASE oldAge", function (err, result) {
+    //     if (err) throw err;
+    //     console.log("Database created");
+    // });
 });
 
-http.createServer((req, res) => {
-    console.log("Server running at port", port)
-    res.end('Hello World!!')
-}).listen(port);
+
+app.get("/", (req, res) => {
+    res.json({hey: "Welcome to my app!"});
+})
+
+app.listen(port, host, () => {
+    console.log("Server running at port", port);
+});
