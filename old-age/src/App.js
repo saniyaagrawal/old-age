417b56bcaf32c272efd573c7fd10f1a5eba9b06f
saniyaagrawal <saniya.agrawal30@gmail.com>
2021-01-29 19:02:33 +0530

    new start

--- old-age/src/App.js
+++ old-age/src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import './App.css';
-import PersistentDrawerLeft from './Components/Header'
-import CustomCarousel from './Components/Carousel'
-import SearchResults from './pages/SearchResults';
-import SpecificPlace from './Components/SpecificPage/SpecificPlace';
+// import PersistentDrawerLeft from './Components/Header'
+// import CustomCarousel from './Components/Carousel'
+// import SearchResults from './pages/SearchResults';
+import Home from './pages/Home';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      {/* <Home /> */}
       {/* <PersistentDrawerLeft />
-      <CustomCarousel/> */}
-      {/* <SearchResults/> */}
-      <SpecificPlace/>
+      <CustomCarousel/>
+      <SearchResults/>
+      <SpecificPlace/> */}
     </div>
   );
 }
---
+++ old-age/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import CustomCarousel from '../Components/Carousel'
+
+function Home() {
+    return (
+        <div>
+            {/* Give Your Loved Ones Quality Care You Can Trust. */}
+            <CustomCarousel />
+        </div>
+    )
+}
+
+export default Home
