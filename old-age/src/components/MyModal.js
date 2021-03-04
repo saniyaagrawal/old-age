f63d2e744c2c5180549dd618036bc90c6fde2384
KrishnamRathi <55155896+KrishnamRathi@users.noreply.github.com>
2021-03-04 14:36:54 +0530

    modal updated (#6)

--- old-age/src/components/MyModal.js
+++ old-age/src/components/MyModal.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react'
 import { Button, Modal } from 'react-bootstrap';
 import '../static/css/mymodal.css'
+import '../static/css/app.css'
 import StaticStars from './StaticStars';
 import BankDetails from './BankDetails';
 import MemberCard from './MemberCard';
+import { DropletHalf, GeoAltFill } from 'react-bootstrap-icons';
+
 
 function MyModal() {
     const [show, setShow] = useState(false);
@@ -27,12 +30,54 @@ function MyModal() {
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <MemberCard/>
+          Ratings (4.0)
+          <StaticStars value={4}/>
+
+          <hr style={{width: '100%', height: 1}} />
+          <div className="oldage_details">
+            <div className="row" style={{alignItems: 'center'}}>
+              <div className="name"><GeoAltFill/>Address:</div>
+              <div>133, xyx some address, Indore (M.P.)</div>
+            </div>
+          </div>
+          <hr style={{width: '100%', height: 1}} />
+          <h4>Facilities</h4>
+          <div className="facilities row" style={{justifyContent: 'space-evenly'}}>
+          <div className="row ai-c">
+              <DropletHalf/>
+              Facility 1
+            </div>
+            <div className="row ai-c">
+              <DropletHalf/>
+              Facility 1
+            </div>
+            <div className="row ai-c">
+              <DropletHalf/>
+              Facility 1
+            </div>
+            <div className="row ai-c">
+              <DropletHalf/>
+              Facility 1
+            </div>  
+            <hr style={{width: '100%', height: 1}} />
+
+          </div>
+          <h4>Members of oldage</h4>
+          <div className="members">
+            <MemberCard/>
+            <MemberCard/>
+            <MemberCard/>
+            <MemberCard/>
+            <MemberCard/>
+            <MemberCard/>
+          </div>
+          <br/>
           <BankDetails/>
+          <hr style={{width: '100%', height: 1}} />
           <div className='review'>Reviews:</div>
           <div>
             <div className='name_stars'>
-                <div className='name'>Saniya Agrawal</div>
+                <div className='name'>person name</div>
                 <div className='stars'><StaticStars value={3}/></div>
             </div>
             <div>This is a good and friendly place.</div>
