aa29745a589e4aa5e66a00e9aa2f139aab6d8e6c
Krishnam rathi <krishnam.rathi@gmail.com>
2021-03-26 13:20:42 +0530

    map fixed

--- old-age/src/pages/SearchResults.js
+++ old-age/src/pages/SearchResults.js
@@ -28,25 +28,24 @@ function SearchResults() {
     })  
     return null
   }
-
   
   if(!old_ages) return <h1>Loading ...</h1>
   else
     return (
-        <div style={{ display: 'flex', height: '100%', marginTop:'50px', position:'fixed',top:0, right:0,left:0, bottom:0 }}>{console.log(lat, longi)}
+        <div style={{ display: 'flex', height: '100%', marginTop:'50px', position:'fixed',top:0, right:0,left:0, bottom:0 }}>
             <SearchList old_ages={old_ages} setLat={setLat} setLongi={setLongi} changeCenter={ChangeCenter} />
             <MapContainer center={[lat, longi]} zoom={13} style={{height: '100%', width: '100%'}}>
                 <TileLayer
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                {old_ages.map(({lat, longi, name}, index) => (
+                {old_ages.map(({lat, longi, name}, index) => lat&&longi ?(
                     <Marker position={[lat, longi] }>
                         <Popup>
                           {name}
                         </Popup>
                     </Marker>
-                ))}
+                ): null )}
             </MapContainer>
         </div>
     )
